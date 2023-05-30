1fea0fe614753826b874319207ad075f4a47d0c4
TheSquashy <thesquashybaker@gmail.com>
2023-05-30 09:55:35 -0400

    wow

--- build/CMakeFiles/3.26.3/CMakeSystem.cmake
+++ build/CMakeFiles/3.26.3/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Linux-6.2.12-1-MANJARO")
+set(CMAKE_HOST_SYSTEM "Linux-6.2.13-1-MANJARO")
 set(CMAKE_HOST_SYSTEM_NAME "Linux")
-set(CMAKE_HOST_SYSTEM_VERSION "6.2.12-1-MANJARO")
+set(CMAKE_HOST_SYSTEM_VERSION "6.2.13-1-MANJARO")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
 include("/home/thesquashy/pico/pico-sdk/cmake/preload/toolchains/pico_arm_gcc.cmake")
--- pwmctl.c
+++ pwmctl.c
@@ -5,6 +5,7 @@
 #include <pico/stdlib.h>
 #include <hardware/gpio.h>
 #include <pico/binary_info.h>
+#include <hardware/pwm.h>
 
 //define our address and some other shit like bitrate.
 const uint LED_PIN = 25;
@@ -12,10 +13,13 @@ static const uint I2C_SLAVE_ADDRESS = 0x17;
 static const uint I2C_BAUDRATE = 100000;
 
 //define what pins were gonna be using. These ones down here VV
-static const uint I2C_SLAVE_SDA_PIN = PICO_DEFAULT_I2C_SDA_PIN;
-static const uint I2C_SLAVE_SCL_PIN = PICO_DEFAULT_I2C_SCL_PIN;
+static const uint I2C_SLAVE_SDA_PIN = 14;
+static const uint I2C_SLAVE_SCL_PIN = 15;
 
-static struct{
+bool run = true;
+
+
+static struct{ // define the struct where we stash our variables
     bool cmdstart;
     uint8_t execute;
     int loc;
@@ -23,27 +27,78 @@ static struct{
     uint8_t cmdbuf[4];
 } context;
 
+const uint pins[] = {0,1,2,3,4,5,6,7};
+const uint dirpins[] = {16,17,18,19,20,21,22,26};
+void startup() {
+    int size = sizeof pins / sizeof pins[0];
+    for(int i = 0; i<size; i++){
+        gpio_set_function(pins[i], GPIO_FUNC_PWM);
+    }
+    size = sizeof dirpins/ sizeof dirpins[0];
+    for(int i =0; i<size;i++){
+        gpio_init(dirpins[i]);
+        gpio_set_dir(dirpins[i], GPIO_OUT);
+    }
+}
+
+
 static void i2c_slave_handler(i2c_inst_t *i2c, i2c_slave_event_t event) {
     switch (event) {
-        case I2C_SLAVE_RECEIVE: // if we get something
-            if(context.loc > 3) {context.cmdstart = false;context.loc=0;context.execute++;memcpy(context.cmdbuf, context.cmd, sizeof(context.cmd));} // if we are beyond the last byte of the string, reset the commandin variable, reset the position variable, and copy the command to a more protected buffer. Then, change the value of the execute flag to let the main loop know it needs to execute a new command.
-            if (context.cmdstart == false) {
-                uint8_t command_id = i2c_read_byte_raw(i2c);
-                context.cmd[context.loc] = command_id;
-                context.cmdstart = true;
-            } else {
-                uint8_t data = i2c_read_byte_raw(i2c);
-                context.cmd[context.loc] = data;
+        case I2C_SLAVE_RECEIVE: // if we start to get something
+            
+            if (context.cmdstart == false) { // and we're not mid command
+                uint8_t command_id = i2c_read_byte_raw(i2c); //receive the byte
+                context.cmd[context.loc] = command_id; //shove it in the buffer
+                context.cmdstart = true; // we're now mid command
+            } else { // we are mid command
+                uint8_t data = i2c_read_byte_raw(i2c); //receive the byte
+                context.cmd[context.loc] = data;//shove it in the buffer
             }
-            context.loc++;
+            context.loc++; // increment the buffer location.
+            break;
+        case I2C_SLAVE_FINISH: // as soon as the write is done
+            context.cmdstart = false; // we're no longer mid-command
+            context.loc=0; // so we go back to the beginning of the buffer
+            context.execute++; // increment the execute flag
+            memcpy(context.cmdbuf, context.cmd, sizeof(context.cmd)); // and copy the completed command to a safer buffer. Old rambly comment below VV
+            break;
+            // if we are beyond the last byte of the string, reset the commandin variable, reset the position variable, and copy the command to a more protected buffer. Then, change the value of the execute flag to let the main loop know it needs to execute a new command.
+        default:
+            context.cmdstart = false; // we're no longer mid-command
+            context.loc=0; // so we go back to the beginning of the buffer
+            context.execute++; // increment the execute flag
+            memcpy(context.cmdbuf, context.cmd, sizeof(context.cmd)); // and copy the completed command to a safer buffer. Old rambly comment below VV
+            break;
     }
     
 }
-bool run = true;
 
+void pwm_handle(uint8_t* array, bool accessory) { // basically i dont feel like rewriting this so just add a flag for wether or not we're controlling accessories.
+    // my brother in christ we need to obtain the pwm speed
+    uint16_t chardelmask = 0b1111111111111100; // use an and to disable the last two bits from the datagram
+    //then post conversion shift everything right two places (by 1)
+    uint8_t d1 = array[2];
+    uint8_t d2 = array[3];
+    uint16_t datagram = ((uint16_t)d1<<8)|d2;
+    uint16_t dirmask = 1 <<1;
+    int direction = (datagram & dirmask) !=0;
+    uint16_t speed = datagram >> 2;
+    speed = speed << 2; // faster x4 multiplication
+    uint8_t num = array[1];
+    for( int p = 0; p < 8; p++){
+        uint8_t bitmask = 1 << p;
+        int bitval = (num & bitmask) !=0;
+        if (bitval) {
+            pwm_set_gpio_level(pins[p], speed);
+            gpio_put(dirpins[p], direction);
+        }
+    }
+        
+}
 
 
 int main() {
+    startup();
     context.execute = 0;
     uint8_t currentid = 0;
     uint8_t localbuf[4];
@@ -53,13 +108,10 @@ int main() {
             memcpy(localbuf,context.cmdbuf,sizeof(context.cmdbuf));
             switch (localbuf[0]) {
                 case 0x11:
-                    pwm_handle(localbuf);
-                    break;
-                case 0x12:
-                    direction_handle(localbuf);
+                    pwm_handle(localbuf, false);
                     break;
                 case 0x13:
-                    lights_handle(localbuf);
+                    pwm_handle(localbuf, true);
                     break;
                 default:
                     break;
